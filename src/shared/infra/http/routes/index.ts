a8a367d40eeddfb1846805ac448b3d364bb9dc94
Marcelo Vilela da Silva <marcelo.vilela.s@gmail.com>
2021-03-09 20:08:48 -0300

    fix(magnet): replace fromFile -> fromURL in real source

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -4,7 +4,6 @@ import technewsRouter from '@modules/technews/infra/http/routes/posts.routes';
 import technewsSourceRouter from '@modules/technewsSource/infra/http/routes/posts.routes';
 //
 import techSourceRouter from '@modules/techSource/infra/http/routes/tech.routes';
-import techRouter from '@modules/tech/infra/http/routes/tech.routes';
 //
 import magnetSourceRouter from '@modules/magnetSource/infra/http/routes/magnet.routes';
 //
@@ -16,7 +15,6 @@ const routes = Router();
 routes.use('/technews', technewsRouter);
 routes.use('/technews-source', technewsSourceRouter);
 
-routes.use('/tech', techRouter);
 routes.use('/tech-source', techSourceRouter);
 
 routes.use('/magnet-source', magnetSourceRouter);
